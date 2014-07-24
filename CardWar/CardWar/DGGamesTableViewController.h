4556bd8543612c360159d690a1f429d13866b89c
Grzegorz Krukiewicz-Gacek <greggscutum@gmail.com>
2014-07-24 22:16:52 +0200

    VCs commit

---
+++ CardWar/CardWar/DGGamesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DGGamesTableViewController.h
+//  CardWar
+//
+//  Created by Grzegorz Krukiewicz-Gacek on 24.07.2014.
+//  Copyright (c) 2014 Estimote. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DGGamesTableViewController : UITableViewController
+
+@end
---
+++ CardWar/CardWar/DGHostViewController.h
@@ -0,0 +1,13 @@
+//
+//  DGHostViewController.h
+//  CardWar
+//
+//  Created by Grzegorz Krukiewicz-Gacek on 24.07.2014.
+//  Copyright (c) 2014 Estimote. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DGHostViewController : UIViewController
+
+@end
